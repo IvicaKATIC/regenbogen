1dc09a3b11d0a7e8ddd88e2d88d124b4716bb931
DerHerrMike <73233016+DerHerrMike@users.noreply.github.com>
2021-12-07 12:15:50 +0100

    Klasse Kind erstellt

---
+++ model/kind.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+use Cassandra\Date;
+
+class Kind
+{
+    public int $id;
+    public string $vorname;
+    public string $nachname;
+    public int $geschlecht;
+    public Date $geburtsdatum;
+    public Date $eintrittsdatum;
+    public int $geschwister;
+    public int $fk_erziehungsberechtigte_id;
+    public int $fk_paedagogen_id;
+
+    public function __construct(int $id, string $vorname,string $nachname, int $geschlecht, Date $geburtsdatum,
+                                Date $eintrittsdatum, int $geschwister, int $fk_erziehungsberechtigte_id, int $fk_paedagogen_id)
+    {
+        $this->id = $id;
+        $this->vorname =$vorname;
+        $this->nachname=$nachname;
+        $this->geschlecht = $geschlecht;
+        $this->geburtsdatum = $geburtsdatum;
+        $this->eintrittsdatum = $eintrittsdatum;
+        $this->geschwister=$geschwister;
+        $this->fk_erziehungsberechtigte_id=$fk_erziehungsberechtigte_id;
+        $this->fk_paedagogen_id=$fk_paedagogen_id;
+    }
+
+}
